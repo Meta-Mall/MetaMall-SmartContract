233091ce138fe96a0289ef831a26616303be2906
Asad Kamran <kamranasad7@gmail.com>
2023-04-02 13:14:13 +0500

    Smart Contract complete rewrite and README.md

--- README.md
+++ README.md
@@ -1,40 +1,28 @@
-# React Truffle Box
+# MetaMall Smart Contract
 
-This box comes with everything you need to start using Truffle to write, compile, test, and deploy smart contracts, and interact with them from a React app.
+This is the official Smart Contract of [Meta-Mall](https://github.com/Meta-Mall)
 
-## Installation
+This contract ensures and manages proper decentralised ownership of shops and stalls in MetaMall through ERC4907 tokens which itself is an extension of ERC721. 
 
-First ensure you are in an empty directory.
+## Requirements to Build
 
-Run the `unbox` command using 1 of 2 ways.
-
+Install truffle if not installed.
 ```sh
 # Install Truffle globally and run `truffle unbox`
 $ npm install -g truffle
-$ truffle unbox react
 ```
 
+Switch to truffle project.
 ```sh
-# Alternatively, run `truffle unbox` via npx
-$ npx truffle unbox react
+$ cd truffle
 ```
 
-Start the react dev server.
-
+Compile contract before deploying to check for any errors. 
 ```sh
-$ cd client
-$ npm start
-  Starting the development server...
+$ truffle compile
 ```
 
-From there, follow the instructions on the hosted React app. It will walk you through using Truffle and Ganache to deploy the `SimpleStorage` contract, making calls to it, and sending transactions to change the contract's state.
-
-## FAQ
-
-- __How do I use this with Ganache (or any other network)?__
-
-  The Truffle project is set to deploy to Ganache by default. If you'd like to change this, it's as easy as modifying the Truffle config file! Check out [our documentation on adding network configurations](https://trufflesuite.com/docs/truffle/reference/configuration/#networks). From there, you can run `truffle migrate` pointed to another network, restart the React dev server, and see the change take place.
-
-- __Where can I find more resources?__
-
-  This Box is a sweet combo of [Truffle](https://trufflesuite.com) and [Create React App](https://create-react-app.dev). Either one would be a great place to start!
+Finally deploy itt to local Blockchain. 
+```sh
+$ truffle migrate
+```
--- truffle/contracts/MetaMall.sol
+++ truffle/contracts/MetaMall.sol
@@ -1,31 +1,63 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract MetaMall {
+import './ERC4907.sol';
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+
+contract MetaMall is ERC4907 {
     
-    //Admin of Contract
+    using Counters for Counters.Counter;
+    Counters.Counter private currentTokenId;
+
     address contractOwner;
 
-    struct UserInfo {
+    struct Store {
+        address owner;
+        uint256 tokenId;
+        uint16 storeNumber;
+        uint256 price;  ///price per month
+        uint256 rent;
+        bool isSaleable;
+        bool isRentable;
+    }
+
+    struct StoreDetails {
+        address owner;
+        uint256 tokenId;
+        uint16 storeNumber;
+        uint256 price;
+        uint256 rent;
+        bool isSaleable;   //sale rent owned
+        bool isRentable;
         address user;
         uint256 expires;
     }
 
-    struct Store {
-        uint256 price;
-        uint256 rent;
-        uint256 area;
-        uint16 storeNumber;
-        address owner;
-        uint256 tokenId;
-        UserInfo currentUser;
-        uint8 status;
-    }
-
-    //give option to owner tos et rent of his land
-
     Store[][] stores;
 
-    constructor() {
+    function getStoreDetails(Store storage store) internal view returns (StoreDetails memory) {
+        StoreDetails memory s = StoreDetails({
+            owner: store.owner,
+            tokenId: store.tokenId,
+            storeNumber: store.storeNumber,
+            price: store.price,
+            rent: store.rent,
+            isSaleable: store.isSaleable,
+            isRentable: store.isRentable,
+            user: _users[store.tokenId].user,
+            expires: _users[store.tokenId].expires
+        });
+        return s; 
+    }
+
+    function mint() internal returns (uint256) {
+        currentTokenId.increment();
+        uint256 newItemId = currentTokenId.current();
+        _safeMint(contractOwner, newItemId);
+        return newItemId;
+    }
+
+    constructor() ERC4907("MetaMall Shops", "LL") {
 
         contractOwner = msg.sender;
         
@@ -37,40 +69,34 @@ contract MetaMall {
         stores.push();
 
         //Floor 0
-        stores[0].push(Store(10, 0, 100, 1, address(0), 1, UserInfo(address(0), 0), 1));
-        stores[0].push(Store(10, 0, 100, 2, address(0), 1, UserInfo(address(0), 0), 1));
-        stores[0].push(Store(10, 0, 100, 3, address(0), 1, UserInfo(address(0), 0), 1));
-        stores[0].push(Store(15, 0, 500, 4, address(0), 5, UserInfo(address(0), 0), 5));
-        stores[0].push(Store(15, 0, 500, 5, address(0), 5, UserInfo(address(0), 0), 5));
-        stores[0].push(Store(20, 0, 700, 6, address(0), 5, UserInfo(address(0), 0), 5));
-        stores[0].push(Store(20, 0, 700, 7, address(0), 5, UserInfo(address(0), 0), 5));
-        stores[0].push(Store(25, 0, 800, 8, address(0), 5, UserInfo(address(0), 0), 5));
-        stores[0].push(Store(25, 0, 800, 9, address(0), 5, UserInfo(address(0), 0), 5));
-        stores[0].push(Store(30, 0, 1000, 10, address(0), 5, UserInfo(address(0), 0), 5));
+        for (uint16 i = 0; i < 32; i++) {
+            stores[0].push(Store(contractOwner, mint(), i, 100, 0, true, false));
+        }
 
         //Floor 1
-        stores[1].push(Store(5, 0, 100, 11, address(0), 1, UserInfo(address(0), 0), 1));
-        stores[1].push(Store(5, 0, 100, 12, address(0), 1, UserInfo(address(0), 0), 1));
-        stores[1].push(Store(5, 0, 100, 13, address(0), 1, UserInfo(address(0), 0), 1));
-        stores[1].push(Store(8, 0, 500, 14, address(0), 5, UserInfo(address(0), 0), 5));
-        stores[1].push(Store(8, 0, 500, 15, address(0), 5, UserInfo(address(0), 0), 5));
+        for (uint16 i = 0; i < 32; i++) {
+            stores[1].push(Store(contractOwner, mint(), i + uint16(stores[0].length), 50, 0, true, false));
+        }
     }
 
+
+    //--------------- F L O O R   M A N A G E M E N T ----------------
+
     function removeFloor() public {
-        require(msg.sender == contractOwner);
-        require(stores[stores.length - 1].length == 0);
+        require(msg.sender == contractOwner, "Not Authorised");
+        require(stores[stores.length - 1].length == 0, "Floor must be empty to be deleted");
 
         stores.pop();
     }
 
     function addFloor() public {
-        require(msg.sender == contractOwner);
+        require(msg.sender == contractOwner, "Not Authorised");
         stores.push();
     }
 
     function addFloorAtIndex(uint index) public {
         //GAS EXPENSIVE
-        require(msg.sender == contractOwner);
+        require(msg.sender == contractOwner, "Not Authorised");
 
         stores.push();
         for(uint i = stores.length - 1; i > index; i--){
@@ -81,7 +107,8 @@ contract MetaMall {
     }
 
     function removeFloorAtIndex(uint index) public {
-        require(stores[index].length == 0);
+        require(msg.sender == contractOwner, "Not Authorised");
+        require(stores[index].length == 0, "Floor must be empty to be deleted");
 
         for(uint i = index; i < stores[index].length - 1; i++){
         stores[index][i] = stores[index][i+1];      
@@ -89,12 +116,155 @@ contract MetaMall {
         stores[index].pop();
     }
 
+    // ------------- G E T T E R   V I E W   F U N C T I O N S -----------------
+
+    function getStore(uint floor, uint index) public view returns (StoreDetails memory) {
+        require(floor < stores.length && index < stores[floor].length, "Invalid Store");
+        return getStoreDetails(stores[floor][index]);
+    }
+
     function getAllStores() public view returns (Store[][] memory) {
         return stores;
     }
 
-    function getStore(uint floor, uint index) public view returns (Store memory) {
-        require(floor < stores.length && index < stores[floor].length);
-        return stores[floor][index];
+    function getOwnedStores(address owner) public view returns (StoreDetails[] memory) {
+        uint count = 0;
+        uint index = 0;
+
+        StoreDetails[] memory owned;
+        for (uint i = 0; i < stores.length; i++) {
+            for (uint j = 0; j < stores[i].length; j++) {
+                if(stores[i][j].owner == owner) {
+                    count++;
+                }
+            }
+        }
+
+        owned = new StoreDetails[](count);
+        for (uint i = 0; i < stores.length; i++) {
+            for (uint j = 0; j < stores[i].length; j++) {
+                if(stores[i][j].owner == owner) {
+                    owned[index++] = getStoreDetails(stores[i][j]);
+                }
+            }
+        }
+        return owned;
+    }
+
+    function getRentedStores() public view returns (StoreDetails[] memory) {
+        uint count = 0;
+        uint index = 0;
+
+        StoreDetails[] memory rented;
+        for (uint i = 0; i < stores.length; i++) {
+            for (uint j = 0; j < stores[i].length; j++) {
+                if(userOf(stores[i][j].tokenId) != address(0)) {
+                    count++;
+                }
+            }
+        }
+
+        rented = new StoreDetails[](count);
+        for (uint i = 0; i < stores.length; i++) {
+            for (uint j = 0; j < stores[i].length; j++) {
+                if(userOf(stores[i][j].tokenId) != address(0)) {
+                    rented[index++] = getStoreDetails(stores[i][j]);
+                }
+            }
+        }
+        return rented;
+    }
+
+    function getAvailableStores() public view returns (StoreDetails[] memory) {
+        uint count = 0;
+        uint index = 0;
+
+        StoreDetails[] memory available;
+        for (uint i = 0; i < stores.length; i++) {
+            for (uint j = 0; j < stores[i].length; j++) {
+                if(stores[i][j].isSaleable || stores[i][j].isRentable) {
+                    count++;
+                }
+            }
+        }
+
+        available = new StoreDetails[](count);
+        for (uint i = 0; i < stores.length; i++) {
+            for (uint j = 0; j < stores[i].length; j++) {
+                if(stores[i][j].isSaleable || stores[i][j].isRentable) {
+                    available[index++] = getStoreDetails(stores[i][j]);
+                }
+            }
+        }
+        return available;
+    }
+
+    // ----------------- S H O P   N F T   M A N A G E M E N T -----------------
+
+    function setRentFee(uint floor, uint storeNumber, uint256 _amountPerMonth) public {
+        require(_isApprovedOrOwner(_msgSender(), stores[floor][storeNumber].tokenId), "Caller is not token owner nor approved");
+        require(_amountPerMonth > 0, "Invalid Price");
+        stores[floor][storeNumber].rent = _amountPerMonth;
+    }
+
+    function setPrice(uint floor, uint storeNumber, uint256 _price) public {
+        require(_isApprovedOrOwner(_msgSender(), stores[floor][storeNumber].tokenId), "Caller is not token owner nor approved");
+        require(_price > 0, "Invalid Price");
+        stores[floor][storeNumber].price = _price;
+    }
+
+    function setRentable(uint floor, uint storeNumber, bool _rentable) public {
+        require(_isApprovedOrOwner(_msgSender(), stores[floor][storeNumber].tokenId), "Caller is not token owner nor approved");
+        require(floor < stores.length && storeNumber < stores[floor].length, "Invalid Store");
+        
+        uint256 tokenId = stores[floor][storeNumber].tokenId;
+
+        require(userOf(tokenId) == address(0), "Store already rented");
+        stores[floor][storeNumber].isRentable = _rentable;
+    }
+
+    function setSaleable(uint floor, uint storeNumber, bool _saleable) public {
+        require(_isApprovedOrOwner(_msgSender(), stores[floor][storeNumber].tokenId), "Caller is not token owner nor approved");
+        require(floor < stores.length && storeNumber < stores[floor].length, "Invalid Store");
+        
+        
+        uint256 tokenId = stores[floor][storeNumber].tokenId;
+        require(userOf(tokenId) == address(0), "Store is rented out. Wait for rent tenure to be over before selling.");
+
+        stores[floor][storeNumber].isSaleable = _saleable;
+    }
+
+    function rent(uint floor, uint storeNumber, uint256 _tokenId, uint64 month) public payable virtual {
+        require(floor < stores.length && storeNumber < stores[floor].length, "Invalid Store");
+
+        uint256 dueAmount = stores[floor][storeNumber].price * month;
+        require(msg.value == dueAmount, "Incorrect amount");
+        require(userOf(_tokenId) == address(0), "Already rented");
+        require(stores[floor][storeNumber].isRentable, "Renting disabled for the NFT");
+        
+        payable(ownerOf(_tokenId)).transfer(dueAmount);
+        UserInfo storage info = _users[_tokenId];
+        info.user = msg.sender;
+        uint64 timeInSeconds = month * 2629746;
+        info.expires = uint256(block.timestamp) + timeInSeconds;//convert months to seconds
+        
+        emit UpdateUser(_tokenId, msg.sender, timeInSeconds);
+    }
+
+    function buy(uint floor, uint storeNumber, uint256 _tokenId) public payable {
+        require(floor < stores.length && storeNumber < stores[floor].length, "Invalid Store");
+
+        require(stores[floor][storeNumber].isSaleable == true, "This land is not availble for sale.");
+        require(msg.value == stores[floor][storeNumber].price, "Incorrect amount");
+
+        payable(ownerOf(_tokenId)).transfer(stores[floor][storeNumber].price);
+        stores[floor][storeNumber].isSaleable = false;
+        stores[floor][storeNumber].isRentable = false;
+
+        safeTransferFrom(
+            stores[floor][storeNumber].owner,
+            msg.sender,
+            stores[floor][storeNumber].tokenId
+        );
     }
 }
